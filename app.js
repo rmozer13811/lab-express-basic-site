aba5de094bda28249f5f8a21c9a4b92ec34672de
Richard Carl Mozer <r_carl1997@hotmail.com>
2021-05-04 02:44:34 -0300

    Solved Lab

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server launched and accessible on http://localhost:3000/");
+});
